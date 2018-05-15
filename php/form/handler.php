fc7771ab4a3282c411e78fbdf51261d378bbf613
Alexander Shibaylo <sasha.s@w-not.ru>
2018-05-15 18:42:50 +0300

    Задачи: - Валидация формы

--- php/form/handler.php
+++ php/form/handler.php
@@ -12,26 +12,24 @@ $birthday_year = $_POST['birthday_year'];
 $url = $_POST['url'];
 $description = $_POST['description'];
 
-if($surname === false) {
-    var_dump($surname);
-}
-var_dump($name);
+var_dump($photo);
+$photo_extension = explode('/', $photo['type']);
+echo
 
-
-if (!$surname || !$name || !$photo || !$birthday_day || !$birthday_month || !$birthday_year) {
+/*if (!$surname || !$name || !$photo || !$birthday_day || !$birthday_month || !$birthday_year) {
     $check = false;
 }
 
-/*$birthday = mktime(0,0,0, $birthday_month, $birthday_day, $birthday_year);
+$birthday = mktime(0,0,0, $birthday_month, $birthday_day, $birthday_year);
 $age18 = strtotime('-18 years');
 if($birthday > $age18) {
   $check = false;
-}*/
+}
 
-/*$http_check = substr($url, 0, 7)
-if( substr($url, 0, 7) !== 'http://' || substr($url, 0, 8) !== 'https://') {
-  $check = false;
+if( substr($url, 0, 7) != 'http://' && substr($url, 0, 8) != 'https://') {
+    $check = false;
 }*/
 
 var_dump($check);
+
 ?>
