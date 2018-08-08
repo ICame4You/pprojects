f422027888a85f4e7db550573831da80bc949ca3
Alexander Shibaylo <sasha.s@w-not.ru>
2018-08-08 18:47:46 +0300

    Circles

---
+++ circles/assets/js/script.js
@@ -0,0 +1,21 @@
+// var rows = 10;
+// var cols = 10;
+// var x = 0;
+// var y = 0;
+//
+// for (var row = 1; row <= rows; row++) {
+//     for (var col = 1; col <= cols; col++ ) {
+//         console.log('test');
+//         var myCircle = new Path.Circle(new Point(x, y), 10);
+//         myCircle.fillColor = 'purple';
+//         x += 100;
+//     }
+//     x = 0;
+//     y += 100;
+// }
+
+// for (var x = 0; x < 1000; x += 100) {
+//     for (var y = 0; y < 1000; y += 100) {
+//         new Path.Circle(new Point(x, y), 10).fillColor = 'purple';
+//     }
+// }
